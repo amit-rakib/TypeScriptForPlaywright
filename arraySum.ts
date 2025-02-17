66e17ece6c0c53acef6aae33bb968854c9be9d85
amit <mr.ahr.es6@gmail.com>
2025-02-18 03:24:30 +0600

    Array sum, reverse and max min

---
+++ arraySum.ts
@@ -0,0 +1,47 @@
+let ars: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+let sum = 0
+
+for(let i = 0; i< ars.length; i++){
+    sum = sum + ars[i]
+}
+
+console.log(sum)
+
+// max and min
+
+let max = ars[0]
+let min = ars[0]
+
+for(let i=1; i<ars.length; i++){
+    if(ars[i]>max){
+        max = ars[i]
+    }
+}
+
+for(let i = 1; i<ars.length; i++){
+    if(ars[i]<min){
+        min = ars[i]
+    }
+}
+
+console.log(`Max: ${max}`)
+console.log(`Min: ${min}`)
+
+// reverse array 
+
+ars = ars.reverse()
+console.log(ars)
+
+// using while loop
+let l = 0
+let r = ars.length
+
+while(l>r){
+   [ars[l], ars[r]] = [ars[r], ars[l]]
+
+   l++
+   r++
+}
+
+console.log(ars)
